2a467abdb37e2a2f3d67690f14546bcb10d3807e
Zuhair Abbas <zuhairabs@gmail.com>
2021-04-25 21:30:13 +0530

    TheBurningDoor

---
+++ adminstats/connect.php
@@ -0,0 +1,5 @@
+<?php
+$dsn='mysql:host=localhost;dbname=blog';//dbname and host
+$username='root';//username
+$password='';//password
+?>
---
+++ database/conn.php
@@ -0,0 +1,4 @@
+<?php
+//database connection
+($GLOBALS["___mysqli_ston"] = mysqli_connect("localhost","root","","blog"));  //host,user,password,database
+?>
